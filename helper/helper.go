6309e1f1e79f4a1b490a5da0c06e46fcd2e51f53
Matius Apry Prastowo <mr.apry.prastowo@gmail.com>
2018-11-29 12:12:39 +0700

    included test helper

---
+++ helper/helper.go
@@ -0,0 +1,224 @@
+package helper
+
+import (
+	"strconv"
+	"time"
+	"crypto/md5"
+	"io"
+	"encoding/hex"
+	"crypto/sha1"
+	"strings"
+	"net/http"
+	"fmt"
+	"math/rand"
+	"encoding/base64"
+	"regexp"
+	"encoding/json"
+)
+
+const (
+	// CHARS for setting short random string
+	CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	// ErrorDataNotFound error message when data doesn't exist
+	ErrorDataNotFound = "data %s not found"
+	// ErrorParameterInvalid error message for parameter is invalid
+	ErrorParameterInvalid = "%s parameter is invalid"
+	// ErrorParameterRequired error message for parameter is missing
+	ErrorParameterRequired = "%s parameter is required"
+	// ErrorParameterLength error message for parameter length is invalid
+	ErrorParameterLength = "length of %s parameter exceeds the limit %d"
+	// ErrorUnauthorized error message for unauthorized user
+	ErrorUnauthorized = "you are not authorized"
+	// SuccessMessage message for success process
+	SuccessMessage = "succeed to process data"
+	// ErrorRedis redis error
+	ErrorRedis = "redis: nil"
+	// ErrorLoginAttempt message for reaching login attempt limit
+	ErrorLoginAttempt = "already reach login attempt limit"
+
+	// FormatDateDB for inserting date to database
+	FormatDateDB = "2006-01-02"
+	//FormatDOB for parsing dob from form value
+	FormatDOB = "02/01/2006"
+	// DefaultDOB default value
+	DefaultDOB = "01/01/0001"
+)
+
+// GetHTTPNewRequest function for getting json API
+func GetHTTPNewRequest(method, url string, body io.Reader, target interface{}, headers ...map[string]string) error {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return err
+	}
+
+	// iterate optional data of headers
+	for _, header := range headers {
+		for key, value := range header {
+			req.Header.Set(key, value)
+		}
+	}
+
+	client := &http.Client{Timeout: 10 * time.Second}
+	r, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	e := json.NewDecoder(r.Body).Decode(target)
+	if e != nil {
+		return e
+	}
+
+	return nil
+}
+
+// GetHTTPNewReqBasicAuth function for getting json API with basic auth
+func GetHTTPNewReqBasicAuth(method, url, userName, password string, body io.Reader, target interface{}, headers ...map[string]string) error {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return err
+	}
+
+	// iterate optional data of headers
+	for _, header := range headers {
+		for key, value := range header {
+			req.Header.Set(key, value)
+		}
+	}
+
+	req.SetBasicAuth(userName, password)
+	client := &http.Client{Timeout: 10 * time.Second}
+	r, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	e := json.NewDecoder(r.Body).Decode(target)
+	if e != nil {
+		return e
+	}
+
+	return nil
+}
+
+// GenerateRandomID function for generating shipping ID
+func GenerateRandomID(length int, prefix ...string) string {
+	var strPrefix string
+
+	if len(prefix) > 0 {
+		strPrefix = prefix[0]
+	}
+
+	yearNow, monthNow, _ := time.Now().Date()
+	year := strconv.Itoa(yearNow)[2:len(strconv.Itoa(yearNow))]
+	month := int(monthNow)
+	RandomString := RandomString(length)
+
+	id := fmt.Sprintf("%s%s%d%s", strPrefix, year, month, RandomString)
+	return id
+}
+
+// RandomString function for random string
+func RandomString(length int) string {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	charsLength := len(CHARS)
+	result := make([]byte, length)
+	for i := 0; i < length; i++ {
+		result[i] = CHARS[rand.Intn(charsLength)]
+	}
+	return string(result)
+}
+
+// RandomStringBase64 function for random string and base64 encoded
+func RandomStringBase64(length int) string {
+	rb := make([]byte, length)
+	_, err := rand.Read(rb)
+
+	if err != nil {
+		return ""
+	}
+	rs := base64.URLEncoding.EncodeToString(rb)
+
+	reg, err := regexp.Compile("[^A-Za-z0-9]+")
+	if err != nil {
+		return ""
+	}
+
+	return reg.ReplaceAllString(rs, "")
+}
+
+// ClearHTML function for validating HTML
+func ClearHTML(src string) string {
+	re, _ := regexp.Compile("\\<[\\S\\s]+?\\>")
+	return re.ReplaceAllString(src, "")
+}
+
+// StringInSlice function for checking whether string in slice
+// str string searched string
+// list []string slice
+func StringInSlice(str string, list []string) bool {
+	for _, v := range list {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
+
+// SetLastName function for setting last name
+func SetLastName(names []string) string {
+	if len(names) <= 1 {
+		return ""
+	}
+
+	names = append(names[:0], names[1:]...)
+
+	return strings.Join(names, " ")
+}
+
+// GenerateMemberID function for generating member id
+// based on member max id
+func GenerateMemberID(lastUserID string) string {
+	preffix := "USR"
+	var strNextID string
+
+	intLastUserID, _ := strconv.Atoi(lastUserID)
+
+	if intLastUserID < 1000 {
+		strNextID = "0" + strconv.Itoa(intLastUserID+1)
+	} else if intLastUserID == 99999999 {
+		strNextID = "00000001"
+	} else {
+		strNextID = strconv.Itoa(intLastUserID + 1)
+	}
+
+	now := time.Now()
+	dt := now.Format("06") + now.Format("01")
+
+	return preffix + dt + strNextID
+}
+
+// GenerateTokenByString function for generate token by string
+func GenerateTokenByString(str string) string {
+	var token string
+
+	// generate random string
+	RandomString := RandomString(30)
+
+	mix := str + "-" + RandomString
+	md := md5.New()
+	io.WriteString(md, mix)
+	sumMd5 := md.Sum(nil)
+	hash := hex.EncodeToString(sumMd5[:])
+
+	sh := sha1.New()
+	io.WriteString(sh, hash)
+	sumSha := sh.Sum(nil)
+	token = hex.EncodeToString(sumSha[:])
+
+	return token
+}
---
+++ helper/helper_test.go
@@ -0,0 +1,262 @@
+package helper
+
+import (
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// PlanktonVariant data structure
+type PlanktonVariant struct {
+	Data []struct {
+		Type       string `json:"type"`
+		ID         string `json:"id"`
+		Attributes Attr   `json:"attributes"`
+	} `json:"data"`
+	Included []struct {
+		OffersProducts
+	} `json:"included"`
+}
+
+// Attr data structure
+type Attr struct {
+	Name          string    `json:"name"`
+	Type          []string  `json:"type"`
+	SKU           string    `json:"skuNo"`
+	RegisterNo    *string   `json:"registerNo"`
+	Status        string    `json:"status"`
+	Note          *string   `json:"note"`
+	Cogs          int       `json:"cogs"`
+	VideoURL      *[]string `json:"videoUrl"`
+	AttachmentURL *[]string `json:"attachmentUrl"`
+	Image         []Images  `json:"images"`
+	Stock         []Stocks  `json:"stock"`
+	Creator       Creators  `json:"creator"`
+	CreatedAt     string    `json:"createdAt"`
+	Editor        Editors   `json:"editor"`
+	ModifiedAt    string    `json:"modifiedAt"`
+}
+
+// Images data structure
+type Images struct {
+	Default bool        `json:"default"`
+	Order   int         `json:"order"`
+	Variety []Varieties `json:"variety"`
+}
+
+// Varieties data structure
+type Varieties struct {
+	Type string `json:"type"`
+	Size string `json:"size"`
+	URL  string `json:"url"`
+}
+
+// Stocks data structure
+type Stocks struct {
+	LocationCode string `json:"locationCode"`
+	Name         string `json:"name"`
+	OnHand       int    `json:"onHand"`
+	OnReserve    int    `json:"onReserve"`
+	Available    int    `json:"available"`
+}
+
+// Creators data structure
+type Creators struct {
+	Email    string `json:"email"`
+	ID       string `json:"id"`
+	IP       string `json:"ip"`
+	JobTitle string `json:"jobTitle"`
+	Name     string `json:"name"`
+}
+
+// Editors data structure
+type Editors struct {
+	Email    string `json:"email"`
+	ID       string `json:"id"`
+	IP       string `json:"ip"`
+	JobTitle string `json:"jobTitle"`
+	Name     string `json:"name"`
+}
+
+// OffersProducts data structure
+type OffersProducts struct {
+	Type       string `json:"type"`
+	ID         string `json:"id"`
+	Attributes Attrs  `json:"attributes"`
+}
+
+// Attrs data structure
+type Attrs struct {
+	SkuNo                 string               `json:"skuNo,omitempty"`
+	VendorSku             *string              `json:"vendorSku,omitempty"`
+	MerchantID            *string              `json:"merchantId,omitempty"`
+	ConditionID           int                  `json:"conditionId,omitempty"`
+	ConditionName         string               `json:"conditionName,omitempty"`
+	ConditionNote         *string              `json:"conditionNote,omitempty"`
+	ShippingLength        *string              `json:"shippingLength,omitempty"`
+	ShippingWidth         *string              `json:"shippingWidth,omitempty"`
+	ShippingHeight        *string              `json:"shippingHeight,omitempty"`
+	ShippingWeight        float32              `json:"shippingWeight,omitempty"`
+	VplPrice              int                  `json:"vplPrice,omitempty"`
+	VplSuggestedPrice     int                  `json:"vplSuggestedPrice,omitempty"`
+	NormalPrice           int                  `json:"normalPrice,omitempty"`
+	SpecialPrice          int                  `json:"specialPrice,omitempty"`
+	SpecialPriceStartDate string               `json:"specialPriceStartDate,omitempty"`
+	SpecialPriceEndDate   string               `json:"specialPriceEndDate,omitempty"`
+	WarrantyTypeName      *string              `json:"warrantyTypeName,omitempty"`
+	WarrantyPeriodID      int                  `json:"warrantyPeriodId,omitempty"`
+	WarrantyPeriodName    *string              `json:"warrantyPeriodName,omitempty"`
+	WarrantyNote          *string              `json:"warrantyNote,omitempty"`
+	OfferInfo             *string              `json:"offerInfo,omitempty"`
+	OfferStatus           string               `json:"offerStatus,omitempty"`
+	HandlingTime          int                  `json:"handlingTime,omitempty"`
+	ShippingNote          string               `json:"shippingNote,omitempty"`
+	Name                  string               `json:"name,omitempty"`
+	Model                 string               `json:"model,omitempty"`
+	Status                string               `json:"status,omitempty"`
+	RegisterNoTypeID      *int                 `json:"registerNoTypeId,omitempty"`
+	RegisterNoTypeName    *string              `json:"registerNoTypeName,omitempty"`
+	ExemptionTypeID       int                  `json:"exemptionTypeId,omitempty"`
+	ExemptionTypeName     *string              `json:"exemptionTypeName,omitempty"`
+	CategoryID            string               `json:"categoryId,omitempty"`
+	CategoryName          *string              `json:"categoryName,omitempty"`
+	CategoryStructure     []CategoryStructures `json:"categoryStructure,omitempty"`
+	BrandID               string               `json:"brandId,omitempty"`
+	BrandName             string               `json:"brandName,omitempty"`
+	Description           []string             `json:"description,omitempty"`
+	InTheBox              []string             `json:"inTheBox,omitempty"`
+	KeyFeatures           *string              `json:"keyFeatures,omitempty"`
+	SearchTerms           *string              `json:"searchTerms,omitempty"`
+	IntendedUse           *string              `json:"intendedUse,omitempty"`
+	TargetAudience        *string              `json:"targetAudience,omitempty"`
+	RealLength            *string              `json:"realLength,omitempty"`
+	RealWidth             *string              `json:"realWidth,omitempty"`
+	RealHeight            *string              `json:"realHeight,omitempty"`
+	RealWeight            float64              `json:"realWeight,omitempty"`
+	Specs                 []Specs              `json:"specs,omitempty"`
+	VariantTypeID         int                  `json:"variantTypeId,omitempty"`
+	VariantTypeName       *string              `json:"variantTypeName,omitempty"`
+	VariantType           *string              `json:"variantType,omitempty"`
+	RichContent           string               `json:"richContent,omitempty"`
+	Creator               Creators             `json:"creator"`
+	CreatedAt             string               `json:"createdAt"`
+	Editor                Editors              `json:"editor"`
+	ModifiedAt            string               `json:"modifiedAt"`
+}
+
+// CategoryStructures data structure
+type CategoryStructures struct {
+	ID           string `json:"id"`
+	OldID        string `json:"oldId"`
+	Name         string `json:"name"`
+	CurrentLevel int    `json:"currentLevel"`
+}
+
+// Specs data structure
+type Specs struct {
+	ID    string `json:"id"`
+	OldID int    `json:"oldId"`
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+// ProductRatingNReview data structure
+type ProductRatingNReview struct {
+	Status string `json:"status"`
+	Data   struct {
+		TotalReview int     `json:"totalReview"`
+		TotalRating float32 `json:"totalRating"`
+	} `json:"data"`
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+// TestGetHTTPNewRequestJSON test function
+//func TestGetHTTPNewRequestJSON(t *testing.T) {
+//	url := "http://api.plankton.bhinneka.com/master/variants?include=offers,product&filter[skuNo]=sku00113580"
+//
+//	headers := map[string]string{
+//		"Content-Type": "application/vnd.api+json",
+//		"Accept":       "application/vnd.plankton_api.v3+json",
+//		"x-api-key":    "74bfb4277e931989784fe54f04ea3951a8631a60",
+//	}
+//
+//	obj := PlanktonVariant{}
+//	if e := GetHTTPNewRequest("GET", url, nil, &obj, headers); e != nil {
+//		assert.Error(t, e)
+//	}
+//}
+
+// TestGetHTTPNewReqBasicAuth test function
+//func TestGetHTTPNewReqBasicAuth(t *testing.T) {
+//	headers := map[string]string{
+//		"Content-Type": "application/vnd.api+json",
+//		"Accept":       "application/vnd.plankton_api.v3+json",
+//		"x-api-key":    "74bfb4277e931989784fe54f04ea3951a8631a60",
+//	}
+//
+//	url := "http://squid.bhinnekalocal.com:9003/api/product-rating/sku00112569"
+//	userName := "bhinneka"
+//	password := "da1c25d8-37c8-41b1-afe2-42dd4825bfea"
+//	obj := ProductRatingNReview{}
+//
+//	if e := GetHTTPNewReqBasicAuth("GET", url, userName, password, nil, &obj, headers); e != nil {
+//		assert.Error(t, e)
+//	}
+//}
+
+// TestClearHTML test function
+func TestClearHTML(t *testing.T) {
+	str := "<strong>Let us make Bhinneka great again</strong>"
+
+	clearStr := ClearHTML(str)
+
+	assert.Equal(t, "Let us make Bhinneka great again", clearStr)
+}
+
+// TestStringInSlice test function
+func TestStringInSlice(t *testing.T) {
+	strSlice := []string{
+		"willy",
+		"bern",
+		"wurry",
+	}
+
+	ok := StringInSlice("willy", strSlice)
+	nok := StringInSlice("linda", strSlice)
+
+	assert.True(t, ok)
+	assert.False(t, nok)
+
+}
+
+// TestRandomString test function
+func TestRandomString(t *testing.T) {
+	length := 8
+	randString := RandomString(length)
+
+	assert.EqualValues(t, length, len(randString))
+}
+
+// TestGenerateRandomID test function
+func TestGenerateRandomID(t *testing.T) {
+	length := 8
+	prefix := "USR"
+
+	randString := GenerateRandomID(length, prefix)
+
+	assert.True(t, strings.Contains(randString, prefix), "random string is false")
+}
+
+func TestRandomStringBase64(t *testing.T) {
+	length := 8
+
+	randString := RandomStringBase64(length)
+
+	reg := regexp.MustCompile("^[A-Za-z0-9]*$")
+	b := reg.Match([]byte(randString))
+
+	assert.True(t, b, "error happens")
+}
